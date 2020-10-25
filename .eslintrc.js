ad109757b26512e3a251d9ec8068b569394b54cc
Anusha Kamat <anushasanyamkamat@gmail.com>
2020-10-25 12:04:28 +0100

    initial vue create commit

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true,
+        semi: false,
+        trailingComma: 'es5',
+      },
+    ],
+    'import/extensions': 0,
+    'global-require': 0,
+    'import/no-dynamic-require': 0,
+    'no-use-before-define': 0,
+    'react/jsx-filename-extension': 0,
+    'react/prop-types': 0,
+    'import/prefer-default-export': 0,
+    'no-underscore-dangle': 0,
+    'no-else-return': 0,
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+}
